a5827c8998569db6c33b514e7fa1c7c007c6eaf5
kapil-gpu <kapil-gpu@DESKTOP-LC4GPCE>
2018-09-23 18:14:10 +0530

    asd

--- proxy/proto.go
+++ proxy/proto.go
@@ -3,21 +3,22 @@ package proxy
 import "encoding/json"
 
 type JSONRpcReq struct {
-	Id     *json.RawMessage `json:"id"`
-	Method string           `json:"method"`
-	Params *json.RawMessage `json:"params"`
+	Id     json.RawMessage `json:"id"`
+	Method string          `json:"method"`
+	Params json.RawMessage `json:"params"`
 }
 
-type JSONRpcReqNH struct  {
-	Id     *json.RawMessage `json:"id"`
-    Method string           `json:"method"`
-	Params *json.RawMessage `json:"params"`
+type JSONRpcReqNH struct {
+	Method string      `json:"method"`
+	Params interface{} `json:"params"`
+	Id     interface{} `json:"id"`
 }
 
 type StratumReq struct {
 	JSONRpcReq
-	Worker string 'json:"worker"`
+	Worker string `json:"worker"`
 }
+
 // Stratum
 type JSONPushMessage struct {
 	// FIXME: Temporarily add ID for Claymore compliance
@@ -27,17 +28,18 @@ type JSONPushMessage struct {
 }
 
 type JSONRpcResp struct {
-	Id      *json.RawMessage `json:"id"`
-	Version string           `json:"jsonrpc"`
-	Result  interface{}      `json:"result"`
-	Error   interface{}      `json:"error,omitempty"
- }
+	Id      json.RawMessage `json:"id"`
+	Version string          `json:"jsonrpc"`
+	Result  interface{}     `json:"result"`
+	Error   interface{}     `json:"error,omitempty"`
+}
 
 type JSONRpcRespNH struct {
-	Id      json.RawMessage `json:"id"`
-	Result  interface{}     `json:"result"`
-	Error   interface{}     `json:"error"`
+	Id     json.RawMessage `json:"id"`
+	Result interface{}     `json:"result"`
+	Error  interface{}     `json:"error"`
 }
+
 type SubmitReply struct {
 	Status string `json:"status"`
 }
